670a73a82a5c8f88bd1ea163ffcc0a904f52a2be
jonatan.hallqvist@molnlycke.com <jonatan.hallqvist@molnlycke.com>
2025-06-11 07:24:57 +0000

    Initialize

---
+++ blank/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["blank/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ blank/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"blank/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
